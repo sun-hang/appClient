0d9bf1681b8d82abfb0f5eea90840bcdd0c77b96
sun <1844528595@qq.com>
2021-04-28 15:35:00 +0800

    test1.8

--- miniprogram/components/details/optionBlock/optionBlock.js
+++ miniprogram/components/details/optionBlock/optionBlock.js
@@ -14,15 +14,42 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    newOptions: []
   },
+  lifetimes: {
+    attached() {
 
+    }
+  },
+  observers: {
+    "options"(options) {
+      let newArr = options.map(item => {
+        item.tag = item.child[0];
+        return item
+      })
+      this.setData({
+        newOptions: newArr
+      })
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
     onClick(e) {
-      this.triggerEvent('change', e);
+      let name = e.target.dataset.name;
+      let index = e.target.dataset.index;
+      let arr = this.data.newOptions;
+      arr.forEach(item => {
+        if (item.name === name) {
+          item.tag = item.child[index];
+        }
+      })
+      this.setData({
+        newOptions: arr
+      })
+      arr = this.data.newOptions.map(item => item.tag).join('-');
+      this.triggerEvent('change', arr);
     }
   }
 })
