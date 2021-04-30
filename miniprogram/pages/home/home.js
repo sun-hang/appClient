207546d205e8242f723faa2fab1abe0efe77a444
sun <1844528595@qq.com>
2021-04-30 20:17:22 +0800

    test2.2

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -41,12 +41,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    wx.getUserInfo({
-      lang: "zh_CN",
-      success(res){
-        console.log(res)
-      }
-    })
+    
   },
 
   /**
