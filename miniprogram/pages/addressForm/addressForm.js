1fbcf92cd3ade10e2e8449b07edc77bfb4d77764
sun-hang <1844528595@qq.com>
2021-05-13 15:15:02 +0800

    test4.0

--- miniprogram/pages/addressForm/addressForm.js
+++ miniprogram/pages/addressForm/addressForm.js
@@ -1,18 +1,71 @@
 // miniprogram/pages/addressForm/addressForm.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    placeholderStyle: "font-size:.7rem;color:#aaa;",
+    placeholderStyle: "font-size:.9rem;color:#aaa;",
+    userName: "",
+    phone: "",
+    pcc: [],
+    detail: "",
+    index: -1
+  },
+  /**
+   * 
+   * @param {Event} e 
+   */
+  onPccChange(e) {
+    this.setData({
+      pcc: e.detail.value
+    })
   },
 
+  /**
+   * 用户名变化事件
+   * @param {Event} e 
+   */
+  onNameChange(e) {
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+
+  /**
+   * 手机号变化事件
+   * @param {Event} e 
+   */
+  onPhoneChange(e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+
+  /**
+   * 详情地址变化事件
+   * @param {Event} e 
+   */
+  onDetailChange(e) {
+    this.setData({
+      detail: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (options.index) {
+      let result = app.globalData.user.address[options.index];
+      this.setData({
+        index: options.index,
+        userName: result.userName,
+        phone: result.phone,
+        pcc: [result.province, result.city, result.county],
+        detail: result.detail
+      })
+    }
   },
 
   /**
